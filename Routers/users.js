bf73367c2e6fbfc6fb296bf23df22a6074f989ae
github_username <mdrayhan9464@gmail.com>
2021-04-28 02:41:50 +0600

    users

--- Routers/users.js
+++ Routers/users.js
@@ -1,108 +1,80 @@
 const express = require("express");
 var bodyParser = require("body-parser");
+const controller = require("./../Controllers/userController");
 
 var jsonParser = bodyParser.json();
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-const db = require("../Models");
-const Model = db.users;
-const Op = db.Sequelize.Op;
-
 const router = new express.Router();
 router.use(express.json());
 
-// todo: Get-All
-router.get("/", async (req, res) => {
-  try {
-    res.send("hello i am Get all Users");
-    console.log("hello i am Get all Users");
-  } catch (e) {
-    res.send(e);
-    console.log("hello i am Get all Users error");
+function responder(res, status, error, data) {
+  if (error || !data) {
+    res.status(status).send({ error, data });
+    console.log("Error", error, data);
+  } else {
+    res.status(status).send(data);
+    console.log("Successfull", data);
   }
-});
-
-// todo: Get-One byid
-router.get("/byid/:id", urlencodedParser, async (req, res) => {
-  try {
-    res.send("hello i am Get one byid Users");
-    console.log("hello i am Get one byid Users");
-  } catch (e) {
-    res.send(e);
-    console.log("hello i am Get one byid Users error");
-  }
-});
-
-// todo: Get-One
-router.get("/byphone/:id", urlencodedParser, async (req, res) => {
-  try {
-    res.send("hello i am Get one byphone Users");
-    console.log("hello i am Get one byphone Users");
-  } catch (e) {
-    res.send(e);
-    console.log("hello i am Get one byphone Users error");
-  }
-});
-
-// todo: Update
-router.put("/:id", urlencodedParser, async (req, res) => {
-  try {
-    res.send("hello i am Update one Users");
-    console.log("hello i am Update one Users");
-  } catch (e) {
-    res.send(e);
-    console.log("hello i am Update one Users error");
-  }
-});
+}
 
 // todo: Create
-router.post("/", urlencodedParser, async (req, res) => {
-  if (!req.body.email) {
-    res.status(400).send({
-      message: "Content can not be empty!",
-    });
-    return;
-  }
+router.post("/", urlencodedParser, (req, res) => {
+  controller.create(req, (status, data, error) => {
+    responder(res, status, error, data);
+  });
+});
 
-  // Create a Tutorial
-  const user = {
-    name: req.body.name,
-    email: req.body.email,
-  };
+// todo: Get-All
+router.get("/", (req, res) => {
+  controller.getAllData((status, data, error) => {
+    responder(res, status, error, data);
+  });
+});
 
-  // Save Tutorial in the database
-  Model.create(user)
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while creating the Tutorial.",
-      });
-    });
+// todo: Get-One byId
+router.get("/byid/:id", urlencodedParser, (req, res) => {
+  controller.findOne(req.params.id, (status, data, error) => {
+    responder(res, status, error, data);
+  });
+});
+
+// todo: Get-One byphone
+router.get("/byphone/:phone_number", urlencodedParser, (req, res) => {
+  controller.findOne(req.params.phone_number, (status, data, error) => {
+    responder(res, status, error, data);
+  });
+});
+
+// todo: Update by id
+router.put("/byid/:id", urlencodedParser, (req, res) => {
+  const updates = req.body;
+  controller.updateOne(req.params.id, updates,(status, data, error) => {
+    responder(res, status, error, data);
+  }); 
+});
+
+// todo: Update many by attribute
+// attributes in query, update in body
+router.put("/byattributes", urlencodedParser, (req, res) => {
+  const updates = req.body;
+  controller.updateMany(req.query, updates,(status, data, error) => {
+    responder(res, status, error, data);
+  }); 
 });
 
 // todo: Delete All
 router.delete("/", urlencodedParser, async (req, res) => {
-  try {
-    res.send("hello i am Delete All Users");
-    console.log("hello i am Delete All Users");
-  } catch (e) {
-    res.send(e);
-    console.log("hello i am Delete All Users error");
-  }
+  controller.deleteAll((status, data, error) => {
+    responder(res, status, error, data);
+  });
 });
 
 // todo: Delete One
-router.delete("/:id", urlencodedParser, async (req, res) => {
-  try {
-    res.send("hello i am Delete One Users");
-    console.log("hello i am Delete One Users");
-  } catch (e) {
-    res.send(e);
-    console.log("hello i am Delete One Users error");
-  }
+router.delete("/byid/:id", urlencodedParser, (req, res) => {
+  controller.deleteOne(req.params.id, (status, data, error) => {
+    responder(res, status, error, data);
+  });
 });
 
 module.exports = router;
